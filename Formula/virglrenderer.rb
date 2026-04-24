33ed3bfcd3f74f8e4da0ff2594ed8e8b30add90f
Sergio Lopez <slp@redhat.com>
2026-04-24 16:29:29 +0200

    virglrenderer: new formula
    Signed-off-by: Sergio Lopez <slp@redhat.com>

---
+++ Formula/virglrenderer.rb
@@ -0,0 +1,24 @@
+class Virglrenderer < Formula
+  desc "VirGL virtual OpenGL renderer"
+  homepage "https://gitlab.freedesktop.org/virgl/virglrenderer"
+  url "https://gitlab.freedesktop.org/slp/virglrenderer/-/archive/0.10.4e-krunkit/virglrenderer-0.10.4e-krunkit.tar.gz"
+  sha256 "09d000623fbdb966cb604eb48c962a0815e8142383e6066d6494809335b76dbb"
+  license "MIT"
+
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libepoxy"
+  depends_on "molten-vk"
+
+  def install
+    args = %w[
+      -Dvenus=true
+      -Drender-server=false
+    ]
+
+    system "meson", "setup", "build", *args, *std_meson_args
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
+  end
+end
