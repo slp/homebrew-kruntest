2da6c58130d153458fd22232c59e70badab7873e
Sergio Lopez <slp@redhat.com>
2026-04-24 16:42:12 +0200

    buildah: new formula
    Closes #4.
    
    Signed-off-by: Sergio Lopez <slp@redhat.com>
    Signed-off-by: Sergio López <511389+slp@users.noreply.github.com>

---
+++ Formula/buildah.rb
@@ -0,0 +1,25 @@
+class Buildah < Formula
+  desc "Tool that facilitates building OCI images"
+  homepage "https://buildah.io"
+  url "https://github.com/containers/buildah/archive/refs/tags/v1.28.0.tar.gz"
+  sha256 "4e406a0cc6a90066cd471deea252fe8862dbd7fa9cb72b274617673d6159a32b"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+  depends_on "go-md2man" => :build
+  depends_on "pkgconf" => :build
+  depends_on "gpgme"
+
+  def install
+    system "make", "bin/buildah", "docs"
+    bin.install "bin/buildah" => "buildah"
+    mkdir_p etc/"containers"
+    etc.install "docs/samples/registries.conf" => "containers/registries.conf"
+    etc.install "tests/policy.json" => "containers/policy.json"
+    man1.install Dir["docs/*.1"]
+  end
+
+  test do
+    assert_match "buildah version", shell_output("#{bin}/buildah -v")
+  end
+end
